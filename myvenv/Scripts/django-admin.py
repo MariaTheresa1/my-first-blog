2f1f146e6ee0b434ce11d9762de8db37d1b306a6
Maria Theresa <mariatheresa98@hotmail.com>
2020-08-22 13:07:02 -0300

    primeiras configurações

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\maria\documents\2020.1\segurança\meuprojeto\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
